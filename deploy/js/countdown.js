533c38da34014fff32b8a21d617140207a6e5dff
OpenShift guest <rtongchenchitt@hotmail.com>
2013-06-04 11:20:22 -0400

    bug fix and color changes

--- deploy/js/countdown.js
+++ deploy/js/countdown.js
@@ -3,42 +3,36 @@ function Countdown(options) {
   instance = this,
   minutes = options.minutes || 0,
   seconds = options.seconds || 5,
-  output = options.output || null,
   updateStatus = options.onUpdateStatus || function () {},
   counterEnd = options.onCounterEnd || function () {};
 
   function decrementCounter() {
-    updateStatus(minutes, seconds);
     if (minutes === 0 && seconds === 0) {
-      counterEnd();
-      instance.stop();
-    } else if (seconds === 0){
-        minutes--;
-        seconds = 59;
+        instance.stop();
+        counterEnd();
     } else {
-        seconds--;
+        if (seconds === 0){
+            minutes--; seconds = 59;
+        } else {
+            seconds--;
+        }
+        updateStatus(minutes, seconds);
     }
+    
   }
 
   this.start = function () {
     clearInterval(timer);
     timer = 0;
+    minutes = options.minutes;
     seconds = options.seconds;
+    updateStatus(minutes, seconds);
     timer = setInterval(decrementCounter, 1000);
   };
 
   this.stop = function () {
     clearInterval(timer);
+    counterEnd();
   };
 }
 
-function terminateCounter(counter){
-    $("#counter").text("Ready to start!");
-    counter.stop();
-    toggleButton($("#stopButton"), $("#startButton"));
-}
-        
-function toggleButton(elementToHide, elementToShow){
-    elementToHide.hide();
-    elementToShow.show();
-}
